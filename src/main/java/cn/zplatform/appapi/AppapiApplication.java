29b5d0706d017c39eae418791f8d8f459d42a9a0
lilac <zhangguochao>
2020-03-30 14:26:16 +0800

    create spring boot project

---
+++ src/main/java/cn/zplatform/appapi/AppapiApplication.java
@@ -0,0 +1,13 @@
+package cn.zplatform.appapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/zplatform/appapi/AppapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zplatform.appapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
