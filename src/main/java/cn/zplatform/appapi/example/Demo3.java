9e1f768a8776b7611a5d9ecd662c00a3c2139acf
lilac <zhangguochao>
2020-03-31 14:28:53 +0800

    删除无效引用

--- src/main/java/cn/zplatform/appapi/example/Demo3.java
+++ src/main/java/cn/zplatform/appapi/example/Demo3.java
@@ -12,13 +12,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
